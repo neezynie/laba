20bc1ac570f78ef5ac1592d1a1dfab15f341ba73
Кубраков Глеб <kubrakov.gleb1@mail.ru>
2024-06-04 14:39:08 +0700

    Аналоговые часы

--- lab8/time.js
+++ lab8/time.js
@@ -3,5 +3,9 @@ function showTime(){
     let currentTime = today.toLocaleString("ru-RU")
     document.getElementById("time").innerHTML = currentTime
     document.getElementById("time1").innerHTML = currentTime
+    let seconds = today.getSeconds();
+    let milliseconds = today.getMilliseconds();
+    let degrees = (seconds * 6) + (milliseconds * 0.006);
+    document.getElementById("Vremy").style.transform = `rotate(${degrees}deg)`;
 }
 setInterval(showTime, 1000)
